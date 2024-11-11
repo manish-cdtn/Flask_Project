860789d2f33f6a65e9d2a49a34725303460e6893
manish <manishkumar@coditation.com>
2024-11-11 18:43:46 +0530

    test commit

--- db_connect.py
+++ db_connect.py
@@ -1,18 +1,48 @@
-from dotenv import dotenv_values
+import json
+import boto3
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import dotenv_values
 
-from config import Config
 
 _DB_SESSION = None
 _DB_ENGINE_SESSION = None
 
 env_vars = dotenv_values()
 def get_db_engine_session():
-    url_str = f'postgresql://postgres:postgres@localhost:5432/postgres'
-    _DB_ENGINE_SESSION = create_engine(url_str, connect_args={'options': '-csearch_path={}'.format('dbo')})
+    """
+    Returns DB connection engine.
+    """
 
-    return _DB_ENGINE_SESSION
+    global _DB_ENGINE_SESSION
+
+    if _DB_ENGINE_SESSION is None:
+
+        secret_name = env_vars.get("SECRET_MANAGER_NAME")
+        region_name = env_vars.get("REGION")
+        db_cluster_name = env_vars.get("CLUSTER_ARN")
+        db_port = env_vars.get("DB_PORT")
+        db_name = env_vars.get("DB_NAME")
+        # Create a Secrets Manager client
+        client = boto3.client('secretsmanager', region_name=region_name)
+
+        # Retrieve the secret value
+        response = client.get_secret_value(SecretId=secret_name)
+
+        # Extract the secret value from the response
+        if 'SecretString' in response:
+            secret_value = json.loads(response['SecretString'])
+            # print("Secret value:", secret_value['dev_github_oauth_token'])
+        else:
+            print('Secret value not found.')
+
+        password = secret_value['password']
+        # print("*****************************", password)
+        url_str = f'postgresql://biopticon:{password}@{db_cluster_name}:{db_port}/{db_name}'
+        # print("************************ ", url_str)
+        _DB_ENGINE_SESSION = create_engine(url_str, connect_args={'options': '-csearch_path={}'.format('dbo')})
+
+        return _DB_ENGINE_SESSION
 
 
 def get_db_session():
--- run.py
+++ run.py
@@ -1,17 +1,18 @@
 from flask import Flask
-from config import Config
 from flask_cors import CORS
 from routes.animal_volume import animal_volume_measurements_bp
 from routes.animal_weight import animal_weight_measurements_bp
  
 
 app = Flask(__name__)
-app.config.from_object(Config)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.register_blueprint(animal_volume_measurements_bp)
 app.register_blueprint(animal_weight_measurements_bp)
 
 
+@app.route("/health")
+def index():
+    return "I AM WORKING FINE"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
